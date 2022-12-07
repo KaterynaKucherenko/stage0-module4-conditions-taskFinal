e8de24a35c7cf10548a198e1f035681e04f44506
KaterynaKucherenko <estorskaya@gmail.com>
2022-12-07 19:35:37 +0300

    fix2

--- src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
@@ -4,13 +4,13 @@ public class CoordinatePane {
     public void printQuadrant(int x, int y) {
 if (x==0 && y==0){
 	        System.out.println( "zero");}
-	        else if ((x == 4 && (y == 0 || y == 1 || y == 2 || y ==3 || y== 4)) || (x==0 && y ==4)){
+	        else if (x == 1 &&  y == 1){
 	            System.out.println("first"); }
-	        else if ((x == 3 && (y == 0 || y == 1 || y == 2 || y ==3))|| (x==0 && y ==3)){
+	        else if (x == -1 &&  y == 1){
 	             System.out.println("second"); }
-	             else if ((x == 2 && (y == 0 || y == 1 || y == 2 ))|| (x==0 && y ==2)){
+	             else if (x == -1 &&  y == -1){
 	             System.out.println("third"); }
-	             else if ((x == 3 && (y == 0 || y == 1 || y == 2 || y ==3))|| (x==0 && y ==1)){
+	             else if (x == 1 &&  y == -1){
 	             System.out.println("fourth"); }
     }
 }
