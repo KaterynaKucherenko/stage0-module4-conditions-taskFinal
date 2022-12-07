de1dada83648966e0182173b6d291f500f775de8
KaterynaKucherenko <estorskaya@gmail.com>
2022-12-07 18:49:19 +0300

    done

--- src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class CoordinatePane {
     public void printQuadrant(int x, int y) {
+if (x==0 && y==0){
+	        System.out.println( "zero");}
+	        else if ((x == 4 && (y == 0 || y == 1 || y == 2 || y ==3 || y== 4)) || (x==0 && y ==4)){
+	            System.out.println("first"); }
+	        else if ((x == 3 && (y == 0 || y == 1 || y == 2 || y ==3))|| (x==0 && y ==3)){
+	             System.out.println("second"); }
+	             else if ((x == 2 && (y == 0 || y == 1 || y == 2 ))|| (x==0 && y ==2)){
+	             System.out.println("third"); }
+	             else if ((x == 3 && (y == 0 || y == 1 || y == 2 || y ==3))|| (x==0 && y ==1)){
+	             System.out.println("fourth"); }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class SeasonDeterminer {
     public void tellTheSeason(int monthNumber) {
+if (month <= 0 || month >13){
+	                    System.out.println("Wrong month number");}
+	    if (month >0 ){
+	        if (month!=0 && (month <=2 || month==12)  ){
+	        System.out.println("Winter");}
+	        if (month>=3 && month <=5){
+	            System.out.println("Spring");}
+	            if (month>=6 && month <=8){
+	            System.out.println("Summer");}
+	            if (month>=9 && month <=11){
+	            System.out.println("Autumn");}}
     }
 }
